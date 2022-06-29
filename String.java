8d948a8d8ce92f9c7b2f8a6f798a013a24470ed6
fahimulislam9 <99759339+fahimulislam9@users.noreply.github.com>
2022-06-29 23:48:46 +0600

    Different operations of String

---
+++ String.java
@@ -0,0 +1,93 @@
+
+package string;
+
+import java.util.Scanner;
+
+public class String {
+
+    
+    public static void main(String[] args) {
+        
+		String_Operation operation = new String_Operation();
+		
+
+		
+
+		String str = "Hello World!";
+		
+		operation.charAt(str);
+		
+		operation.chars(str);
+		
+		operation.codePointAt(str);
+		
+		operation.codePointBefore(str);
+		
+		operation.codePointCount(str);
+		
+		operation.codePoints(str);
+		
+		operation.compareTo(str);
+		
+		operation.compareToIgnoreCase(str);
+		
+		operation.concat(str);
+		
+		operation.contains(str);
+		
+		operation.contentEquals(str);
+		
+		operation.describeConstable(str);
+		
+		operation.endsWith(str);
+		
+		operation.equalsIgnoreCase(str);
+		
+		operation.getBytes(str);
+		
+		operation.getBytes2(str);
+		
+		operation.hashCode(str);
+		
+		operation.indent(str);
+		
+		operation.indexOf(str);
+		
+		operation.indexOf1(str);
+		
+		operation.intern(str);
+		
+		operation.isBlank(str);
+		
+		operation.isEmpty(str);
+		
+		operation.lastIndexOf(str);
+		
+		operation.lastIndexOf1(str);
+		
+		operation.length(str);
+		
+		operation.repeat(str);
+		
+		operation.replace(str);
+		
+		operation.strip(str);
+		
+		operation.stripIndent(str);
+		
+		operation.stripLeading(str);
+		
+		operation.stripTrailing(str);
+		
+		operation.substring(str);
+		
+		operation.toLowerCase(str);
+		
+		operation.toUpperCase(str);
+		
+		operation.trim(str);
+		
+
+	}
+
+}
